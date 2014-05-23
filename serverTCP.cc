f5ae35b61971eb124e373803f1b4e9336a71c770
Arthur Cai <ascai@uwaterloo.ca>
2014-05-23 15:59:19 -0400

    Server comments

--- serverTCP.cc
+++ serverTCP.cc
@@ -19,44 +19,10 @@
 #include "mybind.c"
 #include "unistd.h"
 
-typedef std::map<std::string, std::map<std::string, std::string> > GroupMap;
+#define SELECT_WAIT_SECS 0
+#define SELECT_WAIT_MICROSECS 500000
 
-std::istream & operator>>(std::istream & in, GroupMap & groupMap) {
-	std::string line;
-	std::string groupId;
-	while (std::getline(in, line)) {
-		std::stringstream ss(line);
-		if (line.find_first_of("Group") == 0) {
-			ss >> groupId >> groupId;
-		} else {
-			std::string studentId, studentName;
-			ss >> studentId >> std::ws;
-			std::getline(ss, studentName);
-			groupMap[groupId][studentId] = studentName;
-		}
-	}
-	return in;
-}
-
-struct ClientThread {
-	pthread_t id;
-	int sockfd;
-	GroupMap * groupMap;
-	char * end_session;
-	pthread_mutex_t * m_end_session;
-
-	ClientThread(
-		int sockfd,
-		GroupMap * groupMap,
-		char * end_session,
-		pthread_mutex_t * m_end_session
-	):
-		sockfd(sockfd),
-		groupMap(groupMap),
-		end_session(end_session),
-		m_end_session(m_end_session)
-	{}
-};
+// STRING UTILITIES 
 
 std::string trim(std::string line) {
 	int left, right;
@@ -80,6 +46,51 @@ bool isNumeric(std::string str) {
 	return true;
 }
 
+std::string & tolower(std::string & str) {
+	for (int i = 0; i < str.length(); i++) {
+		if (isupper(str[i])) {
+			str[i] = tolower(str[i]);
+		}
+	}
+	return str;
+}
+
+
+// GROUP MAP
+// student info is stored in this map (groupId -> studentId -> studentName)
+
+typedef std::map<std::string, std::map<std::string, std::string> > GroupMap;
+
+// read stdin input into the groupMap data structure
+std::istream & operator>>(std::istream & in, GroupMap & groupMap) {
+	std::string line;
+	std::string groupId;
+
+	while (std::getline(in, line)) {
+		std::stringstream ss(line);
+		std::string studentId;
+		ss >> studentId >> std::ws;
+
+		if (tolower(studentId) == "group") {
+			// this is a group ID declaration
+			// the next token will be the group ID
+			ss >> groupId;
+		} else {
+			// extract the first token as studentId
+			// the remainder of the line is the studentName
+			std::string studentName;
+			std::getline(ss, studentName);
+			groupMap[groupId][studentId] = studentName;
+		}
+	}
+
+	return in;
+}
+
+
+// INPUT BUFFER
+// Class for translating text sent by the client into server instructions
+
 class InputBuffer {
 	std::stringstream ss;
 	std::string tok;
@@ -88,6 +99,8 @@ class InputBuffer {
 public:
 	InputBuffer(const std::string & str): ss(str) {}
 
+	// Read the next command (contained in the next line of ss)
+	// If there are no more lines to be read, return false; otherwise return true
 	bool next() {
 		get.clear();
 
@@ -99,21 +112,23 @@ public:
 		std::stringstream line_ss(line);
 		if (!(line_ss >> tok)) {
 			tok = "";
-		} else if (tok == "GET") {
-			while (line_ss >> tok) {
-				get.push_back(tok);
-			}
 		} else {
-			tok = trim(line);
+			// Tokenize GET commands
+			if (tolower(tok) == "get") {
+				while (line_ss >> tok) {
+					get.push_back(tok);
+				}
+			}
+			tok = trim(tolower(line));
 		}
 		return true;
 	}
 
 	bool stop() const {
-		return tok == "STOP";
+		return tok == "stop";
 	}
 	bool stopSession() const {
-		return stop() || (tok == "STOP_SESSION");
+		return stop() || (tok == "stop_session");
 	}
 	bool hasGet() const {
 		return isNumeric(getGroupId()) && isNumeric(getStudentId());
@@ -130,10 +145,36 @@ public:
 	}
 };
 
+
+// CLIENT THREAD
+// everything that identifies and will be used by a client-serving thread
+
+struct ClientThread {
+	pthread_t id;						// thread ID
+	int sockfd;							// client socket
+	const GroupMap * groupMap;			// pointer to GroupMap
+	char * end_session;					// shared memory, flag for STOP signal
+	pthread_mutex_t * m_end_session;	// mutex for end_session
+
+	ClientThread(
+		int sockfd,
+		const GroupMap * groupMap,
+		char * end_session,
+		pthread_mutex_t * m_end_session
+	):
+		sockfd(sockfd),
+		groupMap(groupMap),
+		end_session(end_session),
+		m_end_session(m_end_session)
+	{}
+};
+
+// Internal logic for client threads
 void _handle(ClientThread * ct) {
 	char buf[4096];
 
 	while (1) {
+		// Check whether the STOP signal has been sent
 		pthread_mutex_lock(ct->m_end_session);
 		int _end_session = *ct->end_session;
 		pthread_mutex_unlock(ct->m_end_session);
@@ -141,31 +182,39 @@ void _handle(ClientThread * ct) {
 			return;
 		}
 
+		// Use select() here in order to regularly check whether STOP has been sent
 		fd_set fds;
 		FD_ZERO(&fds);
 		FD_SET(ct->sockfd, &fds);
-		timeval tv = {0, 500000};
+		timeval tv = {SELECT_WAIT_SECS, SELECT_WAIT_MICROSECS};
 
 		int retval = select(ct->sockfd + 1, &fds, NULL, NULL, &tv);
 		if (retval < 0) {
+			// Error
 			perror("Select:");
 			return;
 		} else if (retval == 0) {
+			// Nothing to be read from socket
 			continue;
 		}
 
+		// Read from client socket and construct InputBuffer
 		int l = read(ct->sockfd, buf, 4095);
 		buf[l] = '\0';
 		std::string bufstr(buf);
 		InputBuffer inputBuffer(bufstr);
 
 		while (inputBuffer.next()) {
+			// Error case
 			if (inputBuffer.error()) {
 				std::string errStr = "ERROR_INVALID_INPUT";
 				write(ct->sockfd, errStr.c_str(), errStr.length());
+				continue;
 			}
 
+			// STOP case (stop() == true implies stopSession() == true)
 			if (inputBuffer.stop()) {
+				// Communicate to other threads that STOP has been sent
 				pthread_mutex_lock(ct->m_end_session);
 				*ct->end_session = 1;
 				pthread_mutex_unlock(ct->m_end_session);
@@ -174,14 +223,19 @@ void _handle(ClientThread * ct) {
 				return;
 			}
 
+			// GET case
 			if (inputBuffer.hasGet()) {
 				std::string groupId = inputBuffer.getGroupId();
 				std::string studentId = inputBuffer.getStudentId();
-				GroupMap::iterator group = ct->groupMap->find(groupId);
+				// group corresponds to groupMap[groupId]
+				GroupMap::const_iterator group = ct->groupMap->find(groupId);
+				// Check if groupMap[groupId] exists
+				// and groupMap[groupId][studentId] exists
 				if (group != ct->groupMap->end() && group->second.find(studentId) != group->second.end()) {
 					std::string studentName = group->second.find(studentId)->second;
 					write(ct->sockfd, studentName.c_str(), studentName.length());
 				} else {
+					// groupMap[groupId][studentId] does not exist
 					std::stringstream err;
 					err << "ERROR_" << groupId << "_" << studentId;
 					std::string errStr = err.str();
@@ -192,6 +246,8 @@ void _handle(ClientThread * ct) {
 	}
 }
 
+// The handler acting as the main method for the client threads
+// Most of the work is delegated to _handle
 void * handle(void * arg) {
 	ClientThread * ct = (ClientThread *) arg;
 	_handle(ct);
@@ -199,16 +255,22 @@ void * handle(void * arg) {
 	return NULL;
 }
 
-char end_session = 0;
-pthread_mutex_t m_end_session;
 
+// SOCKET UTILITIES
+
+// Provides a sockaddr (complete with IP address)
+// that can be used with AF_INET socket "soc"
+// Returns 0 on success and -1 on error
 int getUnboundSockAddr(int soc, sockaddr_in * addr) {
 	ifaddrs * addrs;
+
+	// Step 1: Get ifaddrs for all network interfaces
 	if (getifaddrs(&addrs) < 0) {
 		perror("getifaddrs():");
 		return -1;
 	}
 
+	// Step 2: Select an open outbound AF_INET interface and get its name
 	std::string ifa_name;
 	for (ifaddrs * addrs_temp = addrs; addrs_temp != NULL; addrs_temp = addrs_temp->ifa_next) {
 		if (addrs_temp->ifa_name == NULL) continue;
@@ -225,6 +287,7 @@ int getUnboundSockAddr(int soc, sockaddr_in * addr) {
 		return -1;
 	}
 
+	// Step 3: Get the IP address for the selected interface
 	ifreq ifr;
 	ifr.ifr_addr.sa_family = AF_INET;
 	strncpy(ifr.ifr_name, ifa_name.c_str(), IFNAMSIZ-1);
@@ -233,12 +296,16 @@ int getUnboundSockAddr(int soc, sockaddr_in * addr) {
 		return -1;
 	}
 
+	// Step 4: Copy info from ioctl to the provided sockaddr pointer
 	*addr = *(sockaddr_in *)(&ifr.ifr_addr);
 	return 0;
 }
 
 
+// MAIN
+
 int main() {
+	// Step 1: Create socket
 	int soc = socket(AF_INET, SOCK_STREAM, 0);
 	
 	if (soc < 0) {
@@ -246,18 +313,21 @@ int main() {
 		return 1;
 	}
 
+	// Step 2: Get the address for an appropriate network interface
 	sockaddr_in addr;
 	if (getUnboundSockAddr(soc, &addr) < 0) {
 		close(soc);
 		return 1;
 	}
 
+	// Step 3: Bind socket to sockaddr
 	if (mybind(soc, &addr) < 0) {
 		perror("Bind:");
 		close(soc);
 		return 1;
 	}
 
+	// Step 4: Open the socket to listen for incoming requests
 	if (listen(soc, 1000) < 0) {
 		perror("Listen:");
 		close(soc);
@@ -266,46 +336,64 @@ int main() {
 
 	std::cout << inet_ntoa(addr.sin_addr) << " " << ntohs(addr.sin_port) << std::endl;
 
+	// Step 5: Construct GroupMap
 	GroupMap groupMap;
 	std::cin >> groupMap;
 
-	std::vector<ClientThread *> threads;
+	char end_session = 0;
+	pthread_mutex_t m_end_session;
 	pthread_mutex_init(&m_end_session, NULL);
-	char _end_session = 0;
 
+	std::vector<ClientThread *> threads;
 	int retCode = 0;
-	while (!_end_session) {
+
+	while (1) {
+		// Step 6: Check whether STOP has been sent
+		pthread_mutex_lock(&m_end_session);
+		int _end_session = end_session;
+		pthread_mutex_unlock(&m_end_session);
+		if (_end_session) {
+			break;
+		}
+
+		// Step 7: Wait for client requests
+		// (select() is used here to continue listening for requests
+		// even while currently serving other clients, and to check
+		// whether STOP has been sent)
 		fd_set fds;
 		FD_ZERO(&fds);
 		FD_SET(soc, &fds);
-		timeval tv = {0, 500000};
+		timeval tv = {SELECT_WAIT_SECS, SELECT_WAIT_MICROSECS};
 
 		int retval = select(soc + 1, &fds, NULL, NULL, &tv);
 		if (retval < 0) {
+			// Error
 			perror("Select:");
 			retCode = 1;
 			break;
-		} else if (retval > 0) {
-			int clientSoc = accept(soc, NULL, NULL);
-			if (clientSoc < 0) {
-				perror("Accept:");
-				retCode = 1;
-				break;
-			}
-
-			ClientThread * ct = new ClientThread(clientSoc, &groupMap, &end_session, &m_end_session);
-			if (pthread_create(&(ct->id), NULL, handle, ct) != 0) {
-				delete ct;
-			} else {
-				threads.push_back(ct);
-			}
+		} else if (retval == 0) {
+			// No client requests, go back to the beginning of the loop
+			continue;
 		}
 
-		pthread_mutex_lock(&m_end_session);
-		_end_session = end_session;
-		pthread_mutex_unlock(&m_end_session);
+		// Step 7: A client wants to connect, accept the connection
+		int clientSoc = accept(soc, NULL, NULL);
+		if (clientSoc < 0) {
+			perror("Accept:");
+			retCode = 1;
+			break;
+		}
+
+		// Step 8: Create new client thread for serving the new client
+		ClientThread * ct = new ClientThread(clientSoc, &groupMap, &end_session, &m_end_session);
+		if (pthread_create(&(ct->id), NULL, handle, ct) != 0) {
+			delete ct;
+		} else {
+			threads.push_back(ct);
+		}
 	}
 
+	// Step 9: Cleanup, join all client threads
 	close(soc);
 	for (unsigned int i = 0; i < threads.size(); ++i) {
 		pthread_join(threads[i]->id, NULL);
--- serverUDP.cc
+++ serverUDP.cc
@@ -17,24 +17,10 @@
 #include "mybind.c"
 #include "unistd.h"
 
-typedef std::map<std::string, std::map<std::string, std::string> > GroupMap;
+#define SELECT_WAIT_SECS 0
+#define SELECT_WAIT_MICROSECS 500000
 
-std::istream & operator>>(std::istream & in, GroupMap & groupMap) {
-	std::string line;
-	std::string groupId;
-	while (std::getline(in, line)) {
-		std::stringstream ss(line);
-		if (line.find_first_of("Group") == 0) {
-			ss >> groupId >> groupId;
-		} else {
-			std::string studentId, studentName;
-			ss >> studentId >> std::ws;
-			std::getline(ss, studentName);
-			groupMap[groupId][studentId] = studentName;
-		}
-	}
-	return in;
-}
+// STRING UTILITIES 
 
 std::string trim(std::string line) {
 	int left, right;
@@ -58,6 +44,51 @@ bool isNumeric(std::string str) {
 	return true;
 }
 
+std::string & tolower(std::string & str) {
+	for (int i = 0; i < str.length(); i++) {
+		if (isupper(str[i])) {
+			str[i] = tolower(str[i]);
+		}
+	}
+	return str;
+}
+
+
+// GROUP MAP
+// student info is stored in this map (groupId -> studentId -> studentName)
+
+typedef std::map<std::string, std::map<std::string, std::string> > GroupMap;
+
+// read stdin input into the groupMap data structure
+std::istream & operator>>(std::istream & in, GroupMap & groupMap) {
+	std::string line;
+	std::string groupId;
+
+	while (std::getline(in, line)) {
+		std::stringstream ss(line);
+		std::string studentId;
+		ss >> studentId >> std::ws;
+
+		if (tolower(studentId) == "group") {
+			// this is a group ID declaration
+			// the next token will be the group ID
+			ss >> groupId;
+		} else {
+			// extract the first token as studentId
+			// the remainder of the line is the studentName
+			std::string studentName;
+			std::getline(ss, studentName);
+			groupMap[groupId][studentId] = studentName;
+		}
+	}
+
+	return in;
+}
+
+
+// INPUT BUFFER
+// Class for translating text sent by the client into server instructions
+
 class InputBuffer {
 	std::stringstream ss;
 	std::string tok;
@@ -66,6 +97,8 @@ class InputBuffer {
 public:
 	InputBuffer(const std::string & str): ss(str) {}
 
+	// Read the next command (contained in the next line of ss)
+	// If there are no more lines to be read, return false; otherwise return true
 	bool next() {
 		get.clear();
 
@@ -77,21 +110,23 @@ public:
 		std::stringstream line_ss(line);
 		if (!(line_ss >> tok)) {
 			tok = "";
-		} else if (tok == "GET") {
-			while (line_ss >> tok) {
-				get.push_back(tok);
-			}
 		} else {
-			tok = trim(line);
+			// Tokenize GET commands
+			if (tolower(tok) == "get") {
+				while (line_ss >> tok) {
+					get.push_back(tok);
+				}
+			}
+			tok = trim(tolower(line));
 		}
 		return true;
 	}
 
 	bool stop() const {
-		return tok == "STOP";
+		return tok == "stop";
 	}
 	bool stopSession() const {
-		return stop() || (tok == "STOP_SESSION");
+		return stop() || (tok == "stop_session");
 	}
 	bool hasGet() const {
 		return isNumeric(getGroupId()) && isNumeric(getStudentId());
@@ -108,10 +143,16 @@ public:
 	}
 };
 
-int handle(int sockfd, GroupMap * groupMap) {
+
+// UDP CLIENT HANDLER
+
+// Internal logic for handling UDP requests
+// Returns 0 on success and non-zero value on error
+int handle(int sockfd, const GroupMap * groupMap) {
 	char buf[4096];
 
 	while (1) {
+		// Listen for and read incoming UDP requests indefinitely
 		sockaddr clientAddr;
 		socklen_t clientAddrLen = sizeof(sockaddr);
 		int l = recvfrom(sockfd, buf, 4095, 0, &clientAddr, &clientAddrLen);
@@ -123,28 +164,41 @@ int handle(int sockfd, GroupMap * groupMap) {
 			return 0;
 		} 
 
+		// UDP request received, construct InputBuffer
 		buf[l] = '\0';
 		std::string bufstr(buf);
 		InputBuffer inputBuffer(bufstr);
 
 		while (inputBuffer.next()) {
+			// Error case
 			if (inputBuffer.error()) {
 				std::string errStr = "ERROR_INVALID_INPUT";
 				sendto(sockfd, errStr.c_str(), errStr.length(), 0, &clientAddr, clientAddrLen);
+				continue;
 			}
 
+			// STOP case (stop() == true implies stopSession() == true)
 			if (inputBuffer.stop()) {
 				return 0;
 			}
+			// UDP server doesn't need to handle STOP_SESSION
+			if (inputBuffer.stopSession()) {
+				continue;
+			}
 
+			// GET case
 			if (inputBuffer.hasGet()) {
 				std::string groupId = inputBuffer.getGroupId();
 				std::string studentId = inputBuffer.getStudentId();
-				GroupMap::iterator group = groupMap->find(groupId);
+				// group corresponds to groupMap[groupId]
+				GroupMap::const_iterator group = groupMap->find(groupId);
+				// Check if groupMap[groupId] exists
+				// and groupMap[groupId][studentId] exists
 				if (group != groupMap->end() && group->second.find(studentId) != group->second.end()) {
 					std::string studentName = group->second.find(studentId)->second;
 					sendto(sockfd, studentName.c_str(), studentName.length(), 0, &clientAddr, clientAddrLen);
 				} else {
+					// groupMap[groupId][studentId] does not exist
 					std::stringstream err;
 					err << "ERROR_" << groupId << "_" << studentId;
 					std::string errStr = err.str();
@@ -155,13 +209,22 @@ int handle(int sockfd, GroupMap * groupMap) {
 	}
 }
 
+
+// SOCKET UTILITIES
+
+// Provides a sockaddr (complete with IP address)
+// that can be used with AF_INET socket "soc"
+// Returns 0 on success and -1 on error
 int getUnboundSockAddr(int soc, sockaddr_in * addr) {
 	ifaddrs * addrs;
+
+	// Step 1: Get ifaddrs for all network interfaces
 	if (getifaddrs(&addrs) < 0) {
 		perror("getifaddrs():");
 		return -1;
 	}
 
+	// Step 2: Select an open outbound AF_INET interface and get its name
 	std::string ifa_name;
 	for (ifaddrs * addrs_temp = addrs; addrs_temp != NULL; addrs_temp = addrs_temp->ifa_next) {
 		if (addrs_temp->ifa_name == NULL) continue;
@@ -178,6 +241,7 @@ int getUnboundSockAddr(int soc, sockaddr_in * addr) {
 		return -1;
 	}
 
+	// Step 3: Get the IP address for the selected interface
 	ifreq ifr;
 	ifr.ifr_addr.sa_family = AF_INET;
 	strncpy(ifr.ifr_name, ifa_name.c_str(), IFNAMSIZ-1);
@@ -186,12 +250,16 @@ int getUnboundSockAddr(int soc, sockaddr_in * addr) {
 		return -1;
 	}
 
+	// Step 4: Copy info from ioctl to the provided sockaddr pointer
 	*addr = *(sockaddr_in *)(&ifr.ifr_addr);
 	return 0;
 }
 
 
+// MAIN
+
 int main() {
+	// Step 1: Create socket
 	int soc = socket(AF_INET, SOCK_DGRAM, 0);
 	
 	if (soc < 0) {
@@ -199,12 +267,14 @@ int main() {
 		return 1;
 	}
 
+	// Step 2: Get the address for an appropriate network interface
 	sockaddr_in addr;
 	if (getUnboundSockAddr(soc, &addr) < 0) {
 		close(soc);
 		return 1;
 	}
 
+	// Step 3: Bind socket to sockaddr
 	if (mybind(soc, &addr) < 0) {
 		perror("Bind:");
 		close(soc);
@@ -213,6 +283,7 @@ int main() {
 
 	std::cout << inet_ntoa(addr.sin_addr) << " " << ntohs(addr.sin_port) << std::endl;
 
+	// Step 4: Construct GroupMap
 	GroupMap groupMap;
 	std::cin >> groupMap;
 
